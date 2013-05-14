75f2703bb02c9518b45e7d4717dfb4138a329e78
Patrick Schafer <pschafer@digipen.edu>
2013-05-14 14:48:57 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing out how to access a users webcam in browser, and having a little cheesy fun while I am at it.
---
+++ js/runtime.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+  navigator.getMedia ({
+    // constraints
+    video: true,
+    },
+
+    // successCallback
+    function(localMediaStream) {
+      var video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(localMediaStream);
+      // Do something with the video here.
+      if (video.mozSrcObject !== undefined) {
+        video.mozSrcObject = localMediaStream;
+      } else {
+        video.src = (window.URL && window.URL.createObjectURL(localMediaStream)) || localMediaStream;
+      }
+      video.play();
+      setInterval(function(){ swap_visible() },5000);
+    },
+
+    // errorCallback
+    function(err) {
+    console.log("The following error occured: " + err);
+  });
+
+  function swap_visible() {
+    $('#camera').hide();
+    $('#fun').show();
+  }
+});
